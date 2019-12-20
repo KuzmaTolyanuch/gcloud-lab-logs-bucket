96ee71d3226e9ee6585eb4bf3288b420ecc13fda
Valerii Kuzmenko <valerii.kuzmenko@refinitiv.com>
2019-12-20 16:07:04 +0200

    Added destroy script

--- destroy_lab.sh
+++ destroy_lab.sh
@@ -2,4 +2,4 @@
 # Get default project id
 PROJECT_NAME=`gcloud config list --format 'value(core.project)'`
 # delete project
-gcloud -q projects delete -y $PROJECT_NAME
+gcloud -q projects delete $PROJECT_NAME
